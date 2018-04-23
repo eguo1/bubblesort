8781c9dbbafd21f530d9322db944f41a70d8bd57
Eric Guo <ericxguo@gmail.com>
2018-04-23 11:15:08 -0500

    implemented a couple of test specs

--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,5 +1,14 @@
+'use strict'
+
 describe('Bubble Sort', function(){
-  it('handles an empty array', function(){
-    expect( bubbleSort([]) ).toEqual( [] );
-  });
-});
+  beforeEach(function () {
+    spyOn(window, 'bubbleSort').and.callThrough()
+  })
+  it('handles an empty array', function (){
+    expect(bubbleSort([])).toEqual([])
+  })
+  it('takes a certain amount of steps to get to the ending', function () {
+    bubbleSort([])
+    expect(bubbleSort.calls.count()).toEqual(1)
+  })
+})
