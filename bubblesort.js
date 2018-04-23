eccd44ac9e7fececb276e5c79ad46b4fa64f6c71
AndrewTrahan <globalART19@gmail.com>
2018-04-23 12:10:30 -0500

    Completed bonus!

--- bubblesort.js
+++ bubblesort.js
@@ -14,3 +14,20 @@ function bubbleSort(arr) {
     return bubbleSort(arr)
   }
 }
+
+function bubbleSortFn(arr, fn) {
+  let tempSwap, swaps = 0;
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (fn(arr[i], arr[i + 1]) === 1) {
+      tempSwap = arr[i]
+      arr[i] = arr[i + 1]
+      arr[i + 1] = tempSwap
+      swaps++
+    }
+  }
+  if (swaps === 0) {
+    return arr
+  } else {
+    return bubbleSortFn(arr, fn)
+  }
+}
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -22,5 +22,37 @@ describe('Bubble Sort', function () {
     expect(bubbleSort.calls.count()).toEqual(4)
   })
 })
+
+describe('Bubble sort w/ callback', function () {
+  const arr1 = [2, 1]
+  const arr2 = [5, 2, 4, 1, 3]
+  function cb(a, b) {
+    if (a > b) {
+      return 1
+    } else if (a < b) {
+      return -1
+    } else {
+      return 0
+    }
+  }
+
+  beforeEach(function () {
+    spyOn(window, 'bubbleSortFn').and.callThrough()
+  })
+  it('handles an empty array', function () {
+    expect(bubbleSortFn([], cb)).toEqual([]);
+    expect(bubbleSortFn.calls.count()).toEqual(1)
+  })
+
+  it('sorts a two element array', function () {
+    expect(bubbleSortFn(arr1, cb)).toEqual([1, 2])
+    expect(bubbleSortFn.calls.count()).toEqual(2)
+  })
+
+  it('sorts a bigger array', function () {
+    expect(bubbleSortFn(arr2, cb)).toEqual([1, 2, 3, 4, 5])
+    expect(bubbleSortFn.calls.count()).toEqual(4)
+  })
+})
 // change
 
