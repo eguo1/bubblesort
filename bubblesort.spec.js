fe98c61437ebb01257ed9728eeb169bff17f6617
AndrewTrahan <globalART19@gmail.com>
2018-04-23 11:15:28 -0500

    my first updates

---
+++ bubblesort.spec.js
@@ -0,0 +1,20 @@
+'use strict'
+
+describe('Bubble Sort', function () {
+  const arr1 = [2, 1]
+  const arr2 = [5, 2, 4, 1, 3]
+
+  beforeEach(function () {
+    spyOn(window, 'bubbleSort').and.callThrough()
+  })
+  it('handles an empty array', function () {
+    expect(bubbleSort([])).toEqual([]);
+    expect(bubbleSort.calls.count()).toEqual(1)
+  })
+
+  it('sorts array', function () {
+    expect(bubbleSort(arr1)).toEqual([2, 1])
+    expect(bubbleSort(arr2)).toEqual([1, 2, 3, 4, 5])
+    expect(bubbleSort.calls.count()).toEqual(5)
+  })
+})
